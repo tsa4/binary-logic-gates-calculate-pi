7b7b68937b03f596a077c6e320bdd5ae4aa21c0c
tsa4 <117873537+tsa4@users.noreply.github.com>
2024-10-01 18:42:17 -0400

    Update calculating_pi.py

--- calculating_pi.py
+++ calculating_pi.py
@@ -8,9 +8,21 @@
 #        k = 1
 from logic import add,subtrac
 def binary_search(f, a, b, tol=1e-7):
-    if f(a) * f(b) >= 0: raise ValueError()
-    while abs(b - a) > tol:
-        mid = (a + b) / 2
-        a, b = (mid, b) if f(mid) * f(a) < 0 else (a, mid)
-    return (a + b) / 2
+    if mult(f(a), f(b)) >= 0:
+        raise ValueError("Function must have different signs at endpoints a and b.")
     
+    while abs(sub(b, a)) > tol:
+        mid = (a + b) >> 1
+        if mult(f(mid), f(a)) < 0:
+            b = mid
+        else:
+            a = mid
+
+    return (a + b) >> 1
+
+def summation(start, end, function):
+    return sum(function(index) for index in range(start, end + 1))
+
+riemann_sqrt = lambda n, k: binary_search(lambda x: mult(x,x)-mult(k,sub(n,k)),1,n)
+
+summation(1,n,riemann_sqrt(n,k))
