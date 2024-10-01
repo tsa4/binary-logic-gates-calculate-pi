b42e81db36b7314ec36b2d52f00e196d347df9e1
tsa4 <117873537+tsa4@users.noreply.github.com>
2024-10-01 17:51:00 -0400

    Update logic.py

--- logic.py
+++ logic.py
@@ -7,42 +7,37 @@ def add(bin_a, bin_b):
         b = carry << 1
     return bin(a)[2:]
 
-def subtract(bin_a, bin_b):
+def sub(bin_a, bin_b):
     a = int(bin_a, 2)
     b = int(bin_b, 2)
+    b = ~b + 1
     while b != 0:
-        borrow = (~a) & b
+        carry = a & b
         a = a ^ b
-        b = borrow << 1
+        b = carry << 1
     return bin(a)[2:]
 
-def multiply(bin_a, bin_b):
+def mult(bin_a, bin_b):
     a = int(bin_a, 2)
     b = int(bin_b, 2)
     result = 0
     while b > 0:
         if b & 1:
-            result = add(bin(result)[2:], bin_a)
+            result ^= a
         a <<= 1
         b >>= 1
     return bin(result)[2:]
 
-def divide(bin_a, bin_b):
+def div(bin_a, bin_b):
     if bin_b == '0':
-        raise ValueError("Cannot divide by zero")
-    
+        raise ValueError("Division by zero is not allowed")
     a = int(bin_a, 2)
     b = int(bin_b, 2)
     quotient = 0
-    remainder = a
-
-    while remainder >= b:
-        temp = b
-        multiple = 1
-        while remainder >= (temp << 1):
-            temp <<= 1
-            multiple <<= 1
-        remainder = subtract(bin(remainder)[2:], bin(temp)[2:])
-        quotient = add(bin(quotient)[2:], bin(multiple)[2:])
-    
-    return bin(quotient)[2:]
+    remainder = 0
+    for i in range(len(bin_a)):
+        remainder = (remainder << 1) | ((a >> (len(bin_a) - 1 - i)) & 1)
+        if remainder >= b:
+            remainder -= b
+            quotient |= (1 << (len(bin_a) - 1 - i))
+    return bin(quotient)[2:], bin(remainder)[2:]
